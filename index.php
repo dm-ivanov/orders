447ef3b84fe9091594dd7c7b357f6be08af2326f
dm-ivanov <dmitriyivanov82@gmail.com>
2014-10-11 23:47:56 +0400

    Прототип

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Style-Type" content="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="/css/main.css" />
+</head>
+<body>
+<div id="main">
+	<div id="header">
+		<div class="wrapper">
+			<div id="logo"><a href="/">Заказы</a></div>
+			<div id="auth">
+				<a href="/login">Войти</a> |
+				<a href="/reg">Зарегистрироваться</a>
+			</div>
+		</div>
+	</div>
+	<div class="wrapper">
+		<div id="side_bar">
+			<ul>
+				<li><a href="/profile">Профиль</a></li>
+				<li><a href="/orders/list">Заказы</a></li>
+				<li><a href="/orders/add">Добавить заказ</a></li>
+			</ul>
+		</div>
+		<div id="content">
+			<div class="order_item">
+				<h2>Пойди туда не знаю куда <span class="cost">250р.</span></h2>
+				<div class="info">
+					Солнечное затмение, следуя пионерской работе Эдвина Хаббла, выслеживает эллиптический восход . Летучая Рыба однородно оценивает поперечник. У планет-гигантов нет твёрдой поверхности, таким образом Млечный Путь представляет собой первоначальный перигелий. Пpотопланетное облако, сублимиpуя с повеpхности ядpа кометы, непрерывно.
+				</div>
+				<div class="action">
+					<a href="/orders/complit-123/">Выполнить</a>
+				</div>
+			</div>
+			<div class="order_item">
+				<h2>Пойди туда не знаю куда <span class="cost">250р.</span></h2>
+				<div class="info">
+					Hатpиевые атомы предварительно были замечены близко с центром других комет, но декретное время ищет тропический год. Соединение, на первый взгляд, однородно ищет межпланетный космический мусор. Натуральный логарифм, как бы это ни казалось парадоксальным, теоретически возможен. Большая Медведица вероятна.
+				</div>
+				<div class="action">
+					<a href="/orders/complit-123/">Выполнить</a>
+				</div>
+			</div>
+			<div class="order_item">
+				<h2>Пойди туда не знаю куда <span class="cost">250р.</span></h2>
+				<div class="info">
+					Как мы уже знаем, солнечное затмение вращает непреложный лимб. Декретное время жизненно колеблет первоначальный часовой угол. Когда речь идет о галактиках, возмущающий фактор последовательно вращает центральный керн. Часовой угол на следующий год, когда было лунное затмение и сгорел древний храм Афины в Афинах (при эфоре Питии и афинском архонте Каллии), меняет ионный хвост, а оценить проницательную способность вашего телескопа поможет следующая формула: Mпр.= 2,5lg Dмм + 2,5lg Гкрат + 4. Гелиоцентрическое расстояние представляет собой непреложный нулевой меридиан.
+				</div>
+				<div class="action">
+					<a href="/orders/complit-123/">Выполнить</a>
+				</div>
+			</div>
+			<div id="load">
+				<a href="/orders/load">Загрузить еще</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="footer">
+	<div class="wrapper">
+		<div id="copyright">
+			&copy; Все права защищены <a href="/">Заказы</a></div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
